21f39ec7461a4c470b786d3e2e33bbab24d811a9
jemuveyan <jemuveyan@axxess.com>
2026-09-06 00:18:22 +0100

    a commit

--- app/src/main/java/com/bellogate_caliphate/chatgpthelper/service/AutomationForegroundService.kt
+++ app/src/main/java/com/bellogate_caliphate/chatgpthelper/service/AutomationForegroundService.kt
@@ -30,7 +30,7 @@ class AutomationForegroundService : Service() {
         private const val CHANNEL_ID = "chatgpt_automation_channel"
         private const val NOTIFICATION_ID = 1001
         private const val CHROME_PACKAGE_NAME = "com.android.chrome"
-        private const val TIMER_INTERVAL_SECONDS = 120
+        private const val TIMER_INTERVAL_SECONDS = 180
     }
 
     private val serviceScope = CoroutineScope(Dispatchers.Main + Job())
