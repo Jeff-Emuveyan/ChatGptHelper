d3fc9d5a54216b5e456316f3920986581b2f8997
jemuveyan <jemuveyan@axxess.com>
2026-09-11 00:19:09 +0100

    a commit

--- app/src/androidTest/java/com/bellogate_caliphate/chatgpthelper/GptUiAutomatorTest.kt
+++ app/src/androidTest/java/com/bellogate_caliphate/chatgpthelper/GptUiAutomatorTest.kt
@@ -21,7 +21,7 @@ class GptUiAutomatorTest {
     companion object {
         private const val TAG = "JEFF_UIAUTOMATOR"
         private const val CHROME_PACKAGE = "com.android.chrome"
-        private const val BATCH_DELAY_MS = 300_000L // 300 seconds (5 minutes) waiting interval
+        private const val BATCH_DELAY_MS = 360_000L // 360 seconds (6 minutes) waiting interval
     }
 
     private lateinit var device: UiDevice
